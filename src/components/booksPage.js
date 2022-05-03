42e2d12fa50a2a34c43fd57c9378164e2b9e62a2
ezekiel utshudi <ezekielutshudi@gmail.com>
2022-05-03 10:45:41 +0200

    file modified extension

--- src/components/booksPage.js
+++ src/components/booksPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import AddNewBook from './AddNewBook';
-import Books from './Books';
+import AddNewBook from './AddNewBook.js';
+import Books from './Books.js';
 
 function BooksPage() {
   const bookList = [
